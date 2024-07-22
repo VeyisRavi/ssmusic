b3c61547db6c528c99c2d8d532c4a52bbf553c5a
Abbasov04 <148398762+Abbasov04@users.noreply.github.com>
2024-07-22 23:19:28 +0400

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None)  # Fill this variable if your upstream repository is private
+    "GIT_TOKEN", "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")  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TuraResmi")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TuraSupportt")
